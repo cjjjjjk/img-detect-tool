2cb6ed9647f220e27e8fd8d120c010535201d3b4
cjjjjjk <ho.vanhai03062004@gmail.com>
2025-08-25 09:21:59 +0700

    update: deployment commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/img-detect-tool'
 };
 
 export default nextConfig;
